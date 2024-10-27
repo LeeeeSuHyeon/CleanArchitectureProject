621af9d166f51efc15fe0dfca61488a7cd64a51c
suhyeon lee <tngus0673@naver.com>
2024-10-27 19:18:07 +0900

    usercase 추가 로직 구현

--- CleanArchitectureProject/Domain/Usecase/UserListUsecase.swift
+++ CleanArchitectureProject/Domain/Usecase/UserListUsecase.swift
@@ -12,6 +12,9 @@ public protocol UserListUsecaseProtocol {
     func getFavoriteUsers() -> Result<[UserListItem], CoreDataError>    // 전체 즐겨찾기 리스트 가져오기
     func saveFavoriteUsers(user : UserListItem) -> Result<Bool, CoreDataError>  // 즐겨찾기 저장
     func deleteFavoriteUsers(userID : Int) -> Result<Bool, CoreDataError>       // 즐겨찾기 삭제
+    func checkFavoriteState(fetchUsers : [UserListItem], favoriteUsers : [UserListItem]) -> [(user : UserListItem, isFavorite : Bool)] // 유저리스트 - 즐겨찾기 포함한 유저인지 구분
+    func convertListToDictionary(favoriteUsers : [UserListItem]) -> [String : [UserListItem]] // 배열 -> Dictionary [초성 : [유저 리스트]]
+
     
     /* 추가해야 할 내용
       1. 배열 -> Dictionary [초성 : [유저 리스트]]
@@ -41,5 +44,21 @@ public struct UserListUsecase : UserListUsecaseProtocol {
     
     public func deleteFavoriteUsers(userID: Int) -> Result<Bool, CoreDataError> {
         repository.deleteFavoriteUsers(userID: userID)
+    }
+    
+    public func checkFavoriteState(fetchUsers: [UserListItem], favoriteUsers: [UserListItem]) -> [(user: UserListItem, isFavorite: Bool)] {
+        let favoriteSet = Set(favoriteUsers) // Set에 들어가기 위해서 UserListItem은 Hashable이어야 함
+        return fetchUsers.map { user in
+            return favoriteSet.contains(user) ? (user : user, isFavorite : true) : (user : user, isFavorite : false)
+        }
+    }
+    
+    public func convertListToDictionary(favoriteUsers: [UserListItem]) -> [String : [UserListItem]] {
+        return favoriteUsers.reduce(into: [String : [UserListItem]]()) { partialResult, user in
+            if let firstString = user.login.first { // 초성
+                let key = String(firstString).uppercased()  // 대문자
+                partialResult[key, default: []].append(user)
+            }
+        }
     }
 }
